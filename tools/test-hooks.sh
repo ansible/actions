03c7257c4d79b744b95b3a01289c7cc41ee0afdc
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2025-10-21 11:43:24 +0100

    fix: ensure uv isolation when custom python is mentioned
    Related: https://github.com/astral-sh/uv/issues/16381
    Related: https://github.com/tox-dev/tox/issues/3632

--- tools/test-hooks.sh
+++ tools/test-hooks.sh
@@ -7,17 +7,4 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-
-set -euo pipefail
-rm -rf .tox/x
-mkdir -p .tox/x
-cd .tox/x
-git init --initial-branch=main
-# we add a file to the repo to avoid error due to no file to to lint
-mkdir -p src
-touch foo.yml
-touch README.md
-touch src/__init__.py
-touch src/bar.py
-git add .
 python3 -m pre_commit try-repo -a -v "${DIR}/.."
