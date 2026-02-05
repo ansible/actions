9f5409d1c879eba54efaabf2d45f422a3f3856e4
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2026-02-05 11:59:20 +0000

    fix: replace pre-commit tool with prek

--- tools/test-hooks.sh
+++ tools/test-hooks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# cspell: ignore prek
 # This scripts checks if ansible-lint works as a hook as expected.
 SOURCE=${BASH_SOURCE[0]}
 while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
@@ -7,4 +8,4 @@ while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-python3 -m pre_commit try-repo -a -v "${DIR}/.."
+prek try-repo -a -v "${DIR}/.."
